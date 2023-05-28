e5308fd008b4cfebf4e8cadf0441ce9e925c1d7e
jesus mtz <jmg357159@gmail.com>
2023-05-28 10:04:59 -0600

    .

--- Inicio.py
+++ Inicio.py
@@ -132,7 +132,7 @@ def tira_basura():
         """
     )
 
-    img_file_buffer = st.camera_input("Take a picture",label_visibility="hidden")
+    img_file_buffer = st.camera_input("Toma una foto",label_visibility="hidden")
 
     if img_file_buffer is not None:
         # To read image file buffer with OpenCV:
